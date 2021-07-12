6e32a569a519fdeed87eca27db8c51f039b1ed93
Abhishek Arora <abbhishek.arora1993@gmail.com>
2021-07-12 16:20:41 +0530

    created first post and it's route

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -9,8 +10,15 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
+        {/* <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1> */}
+
+        <h1 className="title">
+          Read{' '}
+          <Link href="/post/first-post">
+            <a>this page!</a>
+          </Link>
         </h1>
 
         <p className="description">
